4a5e17a17cc066ef758eb9278e527f649e4b7394
Carter Geile <cartergeile@l.local>
2023-01-17 20:08:26 -0600

    created seed and index route

--- server.js
+++ server.js
@@ -35,4 +35,47 @@ mongoose.connection
   app.use(express.static('public'))
   app.use(express.json())
 
-  
+  /* --------- ROUTES -------- */
+  app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+  })
+
+  // BUILD A SEED ROUTE -> make a few animals to start with
+app.get('/animals/seed', (req, res) => {
+  const startAnimals = [
+    {name: 'Bear', color: 'Brown', age: '7'},
+    {name: 'Lion', color: 'Orange', age: '11'},
+    {name: 'Alligator', color: 'Green', age: '2'},
+    {name: 'Elephant', color: 'Grey', age: '27'},
+    {name: 'Zebra', color: 'Black', age: '13'},
+  ]
+  Animal.deleteMany({})
+    .then(() => {
+      Animal.create(startAnimals)
+        .then(data => {
+          res.json(data)
+        })
+        .catch(err => console.log('Error: \n', err))
+    })
+    
+})
+  // INDEX(GET) -> displays all animals
+app.get('/animals', (req, res) => {
+  Animal.find({})
+  .then(animals => { res.json({ animals: animals })})
+  .catch(err => console.log('The following error occurecd: \n', err))
+})
+  // CREATE(POST) -> creates new document in database
+
+  // UPDATE(PUT) -> updates a specific animal
+
+  // DELETE -> delete specific animal
+
+  // SHOW(GET)-> finds and displays single resource
+
+
+
+
+  // Server Listener
+  const PORT = process.env.PORT
+  app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
