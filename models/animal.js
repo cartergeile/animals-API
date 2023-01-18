47f51210eb020454417dd0e246ad3efca1e7455e
Carter Geile <cartergeile@l.local>
2023-01-17 18:49:44 -0600

    made schema and express server

---
+++ models/animal.js
@@ -0,0 +1,19 @@
+// Schema and model for animals resource
+
+const mongoose = require('mongoose')
+
+// destructure Schema and model function from mongoose
+const { Schema, model } = mongoose
+
+// animals Schema
+const animalSchema = new Schema({
+  name: String,
+  color: String,
+  age: Number
+})
+
+// make the model
+const Animal = model('Animal', animalSchema)
+
+// export model
+module.exports = Animal
--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 // import dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+require('dotenv').config()
+const path = require('path')
+
+// import models
+const Animal = require('./models/animal')
+
+// database connection
+// make sure to add to .env
+const DATABASE_URL = process.env.DATABASE_URL
+// db config object
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+// establish db connectioon
+mongoose.connect(DATABASE_URL, CONFIG)
+
+// tell mongoose what to do
+mongoose.connection
+  .on('open', () => console.log('Connected to mongoose'))
+  .on('close', () => console.log('Disconnected from Mongoose'))
+  .on('error', (err) => console.log('An error occured: \n', err))
+
+  // express app object
+  const app = express()
+
+  // middleware
+  app.use(morgan('tiny'))
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.static('public'))
+  app.use(express.json())
+
+  
