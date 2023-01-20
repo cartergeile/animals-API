d3b5ec67ce505b67ac011ffc9e5567639552c293
Carter Geile <cartergeile@l.local>
2023-01-19 20:16:20 -0600

    users can now delete comments

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -13,25 +13,48 @@ router.post('/:animalId', (req, res) => {
   const animalId = req.params.animalId
   if (req.session.loggedIn) {
     req.body.author = req.session.userId
+    const theComment = req.body
+    Animal.findById(animalId)
+    .then(animal => {
+      animal.comments.push(theComment)
+      return animal.save()
+    })
+    .then(animal => {
+      res.status(201).json({ animal: animal })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).json(err)
+    })
   } else {
     res.sendStatus(401)
   }
-  const theComment = req.body
+})
+
+// DELETE `/comments/delete/<animalId>/<commentId>
+router.delete('/delete/:animalId/:commentId', (req, res) => {
+  const { animalId, commentId } = req.params
   Animal.findById(animalId)
-  .then(animal => {
-    animal.comments.push(theComment)
-    return animal.save()
-  })
-  .then(animal => {
-    res.status(201).json({ animal: animal })
+    .then(animal => {
+    const theComment = animal.comments.id(commentId)
+    console.log('this is the comment to be deleted: \n', theComment)
+    if (req.session.loggedIn) {
+      if (theComment.author == req.session.userId) {
+        theComment.remove()
+        animal.save()
+        res.sendStatus(204)
+      } else {
+        res.sendStatus(401)
+      }
+    } else {
+      res.sendStatus(401)
+    }
   })
   .catch(err => {
     console.log(err)
-    res.status(400).json(err)
+    res.sendStatus(400).json(err)
   })
 })
-
-// DELETE
 
 
 
